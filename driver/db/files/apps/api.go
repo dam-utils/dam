44d86b2ac390681c926baaa1ee8efe4c083df921
Kolchyn Alex <kolaa99@mail.ru>
2021-09-03 14:59:02 +0300

    Remove comments

--- driver/db/files/apps/api.go
+++ driver/db/files/apps/api.go
@@ -3,12 +3,6 @@ package apps
 import "os"
 
 type provider struct {
-	//GetApps() []*storage.App
-	//NewApp(app *storage.App)
-	//GetAppById(id int) *storage.App
-	//ExistFamily(family string) bool
-	//RemoveAppById(id int)
-
 	client *os.File
 }
 
